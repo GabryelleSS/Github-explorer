0925af5e43543a027e4c0a808fd1a27fe03fc20c
Gabryelle SS <gabryelless23@gmail.com>
2020-06-30 23:00:49 -0300

    feat: defining global styles

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { DashboardTitle } from './styles';
+
+const Dashboard: React.FC = () => (
+  <DashboardTitle>Expore repositórios no Github</DashboardTitle>
+);
+
+export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const DashboardTitle = styled.h1`
+  font-size: 48px;
+  color: #3A3A3A;
+`;
+
+export default {
+  DashboardTitle
+}
