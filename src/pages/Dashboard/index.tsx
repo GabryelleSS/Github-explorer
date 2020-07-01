edac06e0382e971b090682275f5238aebb85eef5
Gabryelle SS <gabryelle-gaby@hotmail.com>
2020-07-01 00:30:01 -0300

    feat: styling dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,101 @@
 import React from 'react';
-import { DashboardTitle } from './styles';
+
+import { IoIosArrowForward } from 'react-icons/io';
+
+import {
+  Logo,
+  DashboardTitle,
+  Form,
+  InputSearchRepository,
+  ButtonSearchRepository,
+  RepositoriesContainer,
+  Repository,
+  RepositoryDescriptionContainer,
+  UserImage,
+  RepositoryName,
+  RepositoryDescription,
+  RepositoryArrowIcon,
+} from './styles';
+
+import logoImage from '../../assets/img/logo-github.svg';
 
 const Dashboard: React.FC = () => (
-  <DashboardTitle>Expore repositórios no Github</DashboardTitle>
+  <>
+    <Logo src={logoImage} alt="Logo Github Explorer" />
+    <DashboardTitle>Expore repositórios no Github</DashboardTitle>
+
+    <Form>
+      <InputSearchRepository />
+      <ButtonSearchRepository>Pesquisar</ButtonSearchRepository>
+    </Form>
+
+    <RepositoriesContainer>
+      <Repository>
+        <UserImage
+          src="https://avatars1.githubusercontent.com/u/42845859?s=400&u=1d1e4f6a234e5ed703f0187dd1574f07fbe3a733&v=4"
+          alt="Gabryelle SS"
+        />
+        <RepositoryDescriptionContainer>
+          <RepositoryName>gabryelleSS</RepositoryName>
+          <RepositoryDescription>
+            Descrição do repositório
+          </RepositoryDescription>
+        </RepositoryDescriptionContainer>
+
+        <RepositoryArrowIcon>
+          <IoIosArrowForward />
+        </RepositoryArrowIcon>
+      </Repository>
+      <Repository>
+        <UserImage
+          src="https://avatars1.githubusercontent.com/u/42845859?s=400&u=1d1e4f6a234e5ed703f0187dd1574f07fbe3a733&v=4"
+          alt="Gabryelle SS"
+        />
+        <RepositoryDescriptionContainer>
+          <RepositoryName>gabryelleSS</RepositoryName>
+          <RepositoryDescription>
+            Descrição do repositório
+          </RepositoryDescription>
+        </RepositoryDescriptionContainer>
+
+        <RepositoryArrowIcon>
+          <IoIosArrowForward />
+        </RepositoryArrowIcon>
+      </Repository>
+      <Repository>
+        <UserImage
+          src="https://avatars1.githubusercontent.com/u/42845859?s=400&u=1d1e4f6a234e5ed703f0187dd1574f07fbe3a733&v=4"
+          alt="Gabryelle SS"
+        />
+        <RepositoryDescriptionContainer>
+          <RepositoryName>gabryelleSS</RepositoryName>
+          <RepositoryDescription>
+            Descrição do repositório
+          </RepositoryDescription>
+        </RepositoryDescriptionContainer>
+
+        <RepositoryArrowIcon>
+          <IoIosArrowForward />
+        </RepositoryArrowIcon>
+      </Repository>
+      <Repository>
+        <UserImage
+          src="https://avatars1.githubusercontent.com/u/42845859?s=400&u=1d1e4f6a234e5ed703f0187dd1574f07fbe3a733&v=4"
+          alt="Gabryelle SS"
+        />
+        <RepositoryDescriptionContainer>
+          <RepositoryName>gabryelleSS</RepositoryName>
+          <RepositoryDescription>
+            Descrição do repositório
+          </RepositoryDescription>
+        </RepositoryDescriptionContainer>
+
+        <RepositoryArrowIcon>
+          <IoIosArrowForward />
+        </RepositoryArrowIcon>
+      </Repository>
+    </RepositoriesContainer>
+  </>
 );
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,10 +1,122 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Logo = styled.img``;
 
 export const DashboardTitle = styled.h1`
+  color: #3a3a3a;
   font-size: 48px;
-  color: #3A3A3A;
+  line-height: 56px;
+  margin-top: 80px;
+  max-width: 450px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin: 40px 0 80px 0;
+  max-width: 700px;
+`;
+
+export const InputSearchRepository = styled.input.attrs({
+  placeholder: 'Digite o nome do repositório',
+  type: 'text',
+})`
+  background-color: #fff;
+  border: none;
+  border-radius: 5px 0 0 5px;
+  color: #3a3a3a;
+  flex: 1;
+  height: 70px;
+  padding: 0 20px;
+  width: 50%;
+
+  &::placeholder {
+    color: 'a8a8b3';
+  }
+`;
+
+export const ButtonSearchRepository = styled.button.attrs({
+  type: 'submit',
+})`
+  background-color: #04d361;
+  border: none;
+  border-radius: 0 5px 5px 0;
+  color: #fff;
+  font-weight: bold;
+  height: 70px;
+  transition: background-color 0.2s;
+  width: 210px;
+
+  &:hover {
+    background-color: ${shade(0.2, '#04d361')};
+  }
+`;
+
+export const RepositoriesContainer = styled.div`
+  max-width: 700px;
+`;
+
+export const Repository = styled.a.attrs({
+  href: '#',
+})`
+  align-items: center;
+  background-color: #fff;
+  border-radius: 5px;
+  display: flex;
+  padding: 24px;
+  transition: transform 0.2s;
+  text-decoration: none;
+  width: 100%;
+
+  & + a {
+    margin-top: 20px;
+  }
+
+  &:hover {
+    transform: translateX(10px);
+  }
+`;
+
+export const UserImage = styled.img`
+  border-radius: 50%;
+  height: 70px;
+  width: 70px;
+`;
+
+export const RepositoryDescriptionContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  margin-left: 20px;
+`;
+
+export const RepositoryName = styled.strong`
+  color: #3d3d4d;
+  font-size: 20px;
+`;
+
+export const RepositoryDescription = styled.span`
+  color: #a8a8b3;
+  font-size: 18px;
+  margin-top: 5px;
+`;
+
+export const RepositoryArrowIcon = styled.span`
+  color: #cbcbd6;
+  font-size: 16px;
 `;
 
 export default {
-  DashboardTitle
-}
+  Logo,
+  DashboardTitle,
+  Form,
+  InputSearchRepository,
+  ButtonSearchRepository,
+  RepositoriesContainer,
+  Repository,
+  RepositoryDescriptionContainer,
+  UserImage,
+  RepositoryName,
+  RepositoryDescription,
+  RepositoryArrowIcon,
+};
