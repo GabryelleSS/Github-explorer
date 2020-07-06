413d1350b15d52c193f27afdf6ec60e7bf25fa19
Gabryelle SS <gabryelle-gaby@hotmail.com>
2020-07-06 18:37:58 -0300

    fix: styling repositories page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import {
+  Header,
+  Logo,
+  InfoUserContainer,
+  ImageUser,
+  InfoRepoContainer,
+  DescriptionContainer,
+  TitleRepoUser,
+  DescriptionRepo,
+  ListInfo,
+  ItemsListInfo,
+  NumberItems,
+  TitleItems,
+  IssuesContainer,
+  IssuesDescriptionContainer,
+  InfoIssuesContainer,
+  IssuesName,
+  IssuesDescription,
+  RepositoryArrowIcon,
+} from './styles';
+import logoImage from '../../assets/img/logo-github.svg';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +31,63 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <Logo src={logoImage} alt="Logo Github Explorer" />
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            alignItems: 'center',
+            display: 'flex',
+            fontSize: '18px',
+            lineHeight: '2px',
+          }}
+        >
+          <IoIosArrowBack style={{ marginRight: '10px' }} />
+          Voltar
+        </Link>
+      </Header>
+
+      <InfoUserContainer>
+        <InfoRepoContainer>
+          <ImageUser src="https://avatars3.githubusercontent.com/u/42845859?v=4" />
+          <DescriptionContainer>
+            <TitleRepoUser>Gabryelle/OnRuby</TitleRepoUser>
+            <DescriptionRepo>Descrição do repositório</DescriptionRepo>
+          </DescriptionContainer>
+        </InfoRepoContainer>
+        <ListInfo>
+          <ItemsListInfo>
+            <NumberItems>1808</NumberItems>
+            <TitleItems>Stars</TitleItems>
+          </ItemsListInfo>
+          <ItemsListInfo>
+            <NumberItems>48</NumberItems>
+            <TitleItems>Forks</TitleItems>
+          </ItemsListInfo>
+          <ItemsListInfo>
+            <NumberItems>67</NumberItems>
+            <TitleItems>Issues abertas</TitleItems>
+          </ItemsListInfo>
+        </ListInfo>
+      </InfoUserContainer>
+
+      <IssuesContainer>
+        <IssuesDescriptionContainer>
+          <InfoIssuesContainer>
+            <IssuesName>fsfsd ffsdfsd ffsdfsdfh</IssuesName>
+            <IssuesDescription>ffsdfsd</IssuesDescription>
+          </InfoIssuesContainer>
+
+          <RepositoryArrowIcon>
+            <IoIosArrowForward />
+          </RepositoryArrowIcon>
+        </IssuesDescriptionContainer>
+      </IssuesContainer>
+    </>
+  );
 };
 
 export default Repository;
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,130 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  text-transform: lowercase;
+
+  & a {
+    color: #a8a8b2;
+  }
+
+  & a:hover {
+    color: #666;
+  }
+`;
+
+export const Logo = styled.img``;
+
+export const InfoUserContainer = styled.div`
+  margin-top: 50px;
+`;
+
+export const InfoRepoContainer = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export const ImageUser = styled.img`
+  border-radius: 50%;
+  height: 120px;
+  margin-right: 30px;
+  width: 120px;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleRepoUser = styled.strong`
+  color: #3d3d4d;
+  font-size: 36px;
+`;
+
+export const DescriptionRepo = styled.span`
+  color: #737380;
+  font-size: 18px;
+  margin-top: 4px;
+`;
+
+export const ListInfo = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+export const ItemsListInfo = styled.li`
+  display: flex;
+  flex-direction: column;
+  margin: 40px 50px 0 0;
+`;
+
+export const NumberItems = styled.strong`
+  color: #3d3d4d;
+  font-size: 36px;
+`;
+
+export const TitleItems = styled.span`
+  color: #6c6c80;
+`;
+
+export const IssuesContainer = styled.div``;
+
+export const IssuesDescriptionContainer = styled.div`
+  align-items: center;
+  background-color: #fff;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 50px;
+  padding: 35px 24px;
+  transition: transform 0.2s;
+  width: 100%;
+
+  &:hover {
+    transform: translateX(10px);
+  }
+`;
+
+export const InfoIssuesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const IssuesName = styled.strong`
+  color: #3d3d4d;
+  font-size: 20px;
+`;
+
+export const IssuesDescription = styled.span`
+  color: #a8a8b3;
+  font-size: 18px;
+  margin-top: 5px;
+`;
+
+export const RepositoryArrowIcon = styled.div`
+  color: #cbcbd6;
+  font-size: 16px;
+`;
+
+export default {
+  Header,
+  Logo,
+  InfoUserContainer,
+  ImageUser,
+  InfoRepoContainer,
+  DescriptionContainer,
+  TitleRepoUser,
+  DescriptionRepo,
+  ListInfo,
+  ItemsListInfo,
+  NumberItems,
+  TitleItems,
+  IssuesContainer,
+  IssuesDescriptionContainer,
+  InfoIssuesContainer,
+  IssuesName,
+  IssuesDescription,
+  RepositoryArrowIcon,
+};
